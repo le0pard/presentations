d56f9eddafe840cca798191fa51c178582690ce0
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-05-15 16:14:48 -0400

    fix

--- hotcode_postgresql_2013/js/slide-deck.js
+++ hotcode_postgresql_2013/js/slide-deck.js
@@ -125,11 +125,11 @@ SlideDeck.prototype.addEventListeners_ = function() {
   //   'msTransition': 'MSTransitionEnd',
   //   'transition': 'transitionend'
   // };
-  // 
+  //
   // // Find the correct transitionEnd vendor prefix.
   // window.transEndEventName = transEndEventNames[
   //     Modernizr.prefixed('transition')];
-  // 
+  //
   // // When slides are done transitioning, kickoff loading iframes.
   // // Note: we're only looking at a single transition (on the slide). This
   // // doesn't include autobuilds the slides may have. Also, if the slide
@@ -315,7 +315,7 @@ SlideDeck.prototype.loadConfig_ = function(config) {
   }
 
   if (settings.title) {
-    document.title = settings.title.replace(/<br\/?>/, ' ') + ' - PyCon Ukraine 2012';
+    document.title = settings.title.replace(/<br\/?>/, ' ') + ' - HotCode 2013';
     document.querySelector('[data-config-title]').innerHTML = settings.title;
   }
 
@@ -556,7 +556,7 @@ SlideDeck.prototype.updateSlides_ = function(opt_dontPush) {
   this.triggerSlideEvent('slideenter', curSlide);
 
 // window.setTimeout(this.disableSlideFrames_.bind(this, curSlide - 2), 301);
-// 
+//
 // this.enableSlideFrames_(curSlide - 1); // Previous slide.
 // this.enableSlideFrames_(curSlide + 1); // Current slide.
 // this.enableSlideFrames_(curSlide + 2); // Next slide.
