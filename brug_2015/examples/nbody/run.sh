f079fc0f05cabc5872c21d06d25a1ef012688b35
le0pard <leopard.not.a@gmail.com>
2015-11-13 23:57:00 +0300

    nbody

---
+++ brug_2015/examples/nbody/run.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+ITERATIONS=50000000
+
+echo "Cleanup"
+mkdir -p ./.bins
+rm -fr ./.bins/*
+
+echo "Compiling c..."
+gcc -pipe -Wall -O3 -fomit-frame-pointer -march=native -mfpmath=sse -msse3 nbody.c -o nbody -lm
+mv nbody ./.bins/nbody_gcc
+
+echo "Compiling rust..."
+rustc -C opt-level=3 -C target-cpu=core2 nbody.rs
+mv nbody ./.bins/nbody_rust
+
+echo "Compiling go..."
+go build nbody.go
+mv nbody ./.bins/nbody_go
+
+echo "Compiling crystal..."
+crystal build nbody.cr --release
+mv nbody ./.bins/nbody_cr
+
+echo "Benchmarking"
+
+echo "C time"
+time ./.bins/nbody_gcc $ITERATIONS
+echo "Rust time"
+time ./.bins/nbody_rust $ITERATIONS
+echo "Golang time"
+time ./.bins/nbody_go $ITERATIONS
+echo "Crystal time"
+time ./.bins/nbody_cr $ITERATIONS
+
+echo "Finished work"
